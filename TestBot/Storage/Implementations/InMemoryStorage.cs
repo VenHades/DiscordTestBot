8b941d47f47dee40f1b3bac4699d3571bf9c9ada
VHades <psi_alexn@yahoo.com>
2018-07-20 22:36:25 -0400

    asd

---
+++ TestBot/Storage/Implementations/InMemoryStorage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TestBot.Storage.Implementations
+{
+    public class InMemoryStorage : IDataStorage
+    {
+        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+        public T RestoreObject<T>(string key)
+        {
+            if (!_dictionary.ContainsKey(key)) throw new System.Exception            
+        }
+
+        public void StoreObject(object obj, string key)
+        {
+            if (_dictionary.ContainsKey(key)) return;
+            _dictionary.Add(key, obj);
+        }
+    }
+}
